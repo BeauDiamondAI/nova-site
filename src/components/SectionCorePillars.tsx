5b65df4f36edf8517d3e607bcca9e723612f9380
EC2 Default User <ec2-user@ip-172-31-19-50.ec2.internal>
2025-05-13 16:19:29 +0000

    Trigger redeploy

--- src/components/SectionCorePillars.tsx
+++ src/components/SectionCorePillars.tsx
@@ -115,7 +115,7 @@ export default function SectionCorePillars() {
             />
   
             {/* Tag in top-left */}
-            <div className="absolute top-4 left-4 bg-black/60 px-3 py-1 rounded-md text-xs text-teal-400 font-mono z-10 tracking-wide lowercase normal-case">
+            <div className="absolute top-4 left-4 bg-black/60 px-3 py-1 rounded-md text-xs text-teal-400 font-[monospace] z-10 tracking-wide lowercase"            >
               {card.tag}
             </div>
   
