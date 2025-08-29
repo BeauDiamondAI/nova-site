0f097e7742c5cf53b862f7c1f14548fce54e5ea6
EC2 Default User <ec2-user@ip-172-31-19-50.us-east-1.compute.internal>
2025-08-29 16:14:07 +0000

    Font change 1

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,6 +5,7 @@ module.exports = {
     extend: {
       fontFamily: {
         headline: ['var(--font-headline)'],
+        sans: ['Borna', '-apple-system', 'BlinkMacSystemFont', 'Segoe UI', 'Roboto', 'sans-serif'],
       },
       animation: {
         'fade-in': 'fadeIn 4s ease-out forwards',
