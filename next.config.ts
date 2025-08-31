42ebbd94f8fb3e4de040bf9019e383e0a9548f1d
EC2 Default User <ec2-user@ip-172-31-19-50.us-east-1.compute.internal>
2025-08-31 02:59:39 +0000

    New compressed hero and WhatsNext background 1.3

--- next.config.ts
+++ next.config.ts
@@ -5,3 +5,15 @@ const nextConfig: NextConfig = {
 };
 
 export default nextConfig;
+
+module.exports = {
+  async headers() {
+    return [
+      {
+        source: '/images/(.*)',
+        headers: [{ key: 'Cache-Control', value: 'public, max-age=31536000, immutable' }]
+      }
+    ];
+  }
+}
+
